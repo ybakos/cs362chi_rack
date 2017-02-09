8ce27002cb09712bec37561ed7be979e89cc7239
Hanna Bennett <bennetthanna22@gmail.com>
2017-02-08 16:33:37 -0800

    Screen now prints out title, year, and rank in array format

--- main.rb
+++ main.rb
@@ -1,19 +1,56 @@
 require "rack"
 
+class Album
+
+	attr_reader :title, :rank, :year
+
+	def initialize(title, year, rank)
+		@title = title
+		@rank = rank
+		@year = year
+	end
+
+  def to_s
+    "#{title} #{rank} #{year}"
+  end
+
+  def self.build(array)
+    albums = []
+
+    albums = array.map.with_index do |d, i|
+      components = d.split(",")
+      Album.new(components[0], components[1], i + 1)
+    end
+    return albums
+  end
+
+  def self.print_data(array)
+    info = []
+    array.each do |album|
+      info << album.title
+      info << album.year
+      info << album.rank
+    end
+    return info
+  end
+end
+
 class AlbumsApp
 	def call(env)
-		albums = []
+		data = []
 
 		File.open("top_100_albums.txt", "r") do |file|
-			albums = file.readlines
+			data = file.readlines
 		end
+
+    albums = Album.build(data)
+    info = Album.print_data(albums)
 
 		# albums.each do |album|
 		# 	puts album
 		# end
-		['200', {'Content-type' => 'text/html'}, [albums.to_s]]
+		['200', {'Content-type' => 'text/html'}, [info.to_s]]
 	end
-
 end
 
 #Rack::Handler::WEBrick.run AlbumsApp.new
