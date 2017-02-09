c1c8736d87841e2ee36848b686f164d559097424
Hanna Bennett <bennetthanna22@gmail.com>
2017-02-08 17:16:35 -0800

    Not working but attempt to link index.html.erb file

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require "rack"
+require "erb"
 
 class Album
 
@@ -24,19 +25,26 @@ class Album
     return albums
   end
 
-  def self.print_data(array)
-    info = []
-    array.each do |album|
-      info << album.title
-      info << album.year
-      info << album.rank
-    end
-    return info
-  end
+  # def self.print_data(array)
+  #   info = []
+  #   array.each do |album|
+  #     info << album.title
+  #     info << album.year
+  #     info << album.rank
+  #   end
+  #   return info
+  # end
 end
 
 class AlbumsApp
 	def call(env)
+
+    request = Rack::Request.new(env)
+    case request.path
+    when "/" then Rack::Response.new(render("index.html.erb"))
+    else Rack::Response.new("Not Found", 404)
+    end
+
 		data = []
 
 		File.open("top_100_albums.txt", "r") do |file|
@@ -44,13 +52,19 @@ class AlbumsApp
 		end
 
     albums = Album.build(data)
-    info = Album.print_data(albums)
+    # info = Album.print_data(albums)
 
 		# albums.each do |album|
 		# 	puts album
 		# end
-		['200', {'Content-type' => 'text/html'}, [info.to_s]]
+		['200', {'Content-type' => 'text/html'}, [albums.to_s]]
 	end
+
+  def render(template)
+    path = File.expand_path("#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+
 end
 
 #Rack::Handler::WEBrick.run AlbumsApp.new
