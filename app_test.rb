bebc9ad68cd14dad35c5b338e62194e1d8d367a9
Yong Bakos <ybakos@humanoriented.com>
2017-02-11 22:29:32 -0800

    Add comments for improvement, and to harass students with love.

--- app_test.rb
+++ app_test.rb
@@ -1,16 +1,16 @@
-ENV['RACK_ENV'] = 'test'
+ENV['RACK_ENV'] = 'test' # Super great start, now... what should be tested? (What are the requirements?)
 
 require './lib/main.rb'
 require 'rack/test'
 require 'test/unit'
- 
+
 class AlbumsAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
- 
+
   def app
     AlbumRankApp.new
   end
- 
+
   def test_header_is_passed_correctly
     get '/'
     assert last_response.ok?
@@ -28,5 +28,5 @@ class AlbumsAppTest < Test::Unit::TestCase
     assert last_response.not_found?
     assert_equal 404, last_response.status
   end
- 
+
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,33 +1,33 @@
-require "rack"
+require "rack" # Prefer single ticks.
 require "erb"
 
-class Album
-  attr_accessor :rank, :albumName, :year
+class Album # One blank line after this.
+  attr_accessor :rank, :albumName, :year # Do we say that people have "peopleNames"?
 
   def initialize(rank, albumName, year)
     @rank = rank
     @albumName = albumName
     @year = year
-  end
+  end # One blank line after this.
 end
 
 
 class AlbumRank
 
-  def self.build_array(array)
+  def self.build_array(array) # Omit the next blank line.
 
-    rankArray = []
-
-    rankArray = array.map.with_index do |d, i|
-      albumName, year = d.split(",")
-      year = year.to_i
+    rankArray = []  # Rubyists use lower_snake_case. This variable isn't necessary at all, because...
+                              # I don't think this blank line belongs here.
+    rankArray = array.map.with_index do |d, i|  # ... map returns an array anyway.
+      albumName, year = d.split(",") # album_name. Or how about just name?
+      year = year.to_i # Hmmm, why convert this to an Integer if you're never going to perform int operations on it?
       Album.new(i+1, albumName, year)
-    end 
+    end
     return rankArray
   end
 
 
-  def print_data
+  def print_data # Interesting. So I have to instantiate an AlbumRank object in order to call this?
     rankArray.each do |item|
     	puts item.inspect
     end
@@ -40,7 +40,7 @@ class AlbumRankApp
   def call(env)
     request = Rack::Request.new(env)
 
-    albums = []
+    albums = [] # Note that every request will hit the filesystem and load the data from disk. (Hint: constructor)
 
     File.open("top_100_albums.txt", "r") do |file|
       albums = file.readlines
@@ -49,19 +49,19 @@ class AlbumRankApp
     @rankedAlbums = AlbumRank.build_array(albums)
     # This is our front controller!
     case request.path
-    when "/" then 
-      Rack::Response.new(render("index.html.erb", @rankedAlbums))
+    when "/" then
+      Rack::Response.new(render("index.html.erb", @rankedAlbums)) # You must like repeated code, repeated code, repeated code.
 
     when "/orderByAlbumNameLength" then
-      @rankedAlbums.sort! { |a,b| a.albumName.length <=> b.albumName.length }
-      Rack::Response.new(render("index.html.erb", @rankedAlbums))
+      @rankedAlbums.sort! { |a,b| a.albumName.length <=> b.albumName.length } # See sort_by. And there's another trick using &...
+      Rack::Response.new(render("index.html.erb", @rankedAlbums)) # You must like repeated code, repeated code, repeated code.
 
     when "/orderByYear" then
-      @rankedAlbums.sort! { |a,b| a.year <=> b.year }
-      Rack::Response.new(render("index.html.erb", @rankedAlbums))
+      @rankedAlbums.sort! { |a,b| a.year <=> b.year } # See sort_by. And there's another trick using &...
+      Rack::Response.new(render("index.html.erb", @rankedAlbums)) # ... :)
 
     when "/orderAlphabetically" then
-      @rankedAlbums.sort! { |a,b| a.albumName <=> b.albumName }
+      @rankedAlbums.sort! { |a,b| a.albumName <=> b.albumName } # See sort_by. And there's another trick using &...
       Rack::Response.new(render("index.html.erb", @rankedAlbums))
 
     else Rack::Response.new("404 Not Found", 404)
@@ -72,5 +72,5 @@ class AlbumRankApp
     path = File.expand_path("../views/#{template}", __FILE__)
     output = ERB.new(File.read(path))
     output.result(binding)
-  end
+  end # One blank line after this.
 end
