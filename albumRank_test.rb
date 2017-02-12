3def2017ad5834802d674a031f1a97d0f79bd445
Jack Wilson <Jack Wilson>
2017-02-12 10:46:14 -0800

    tests for albumrank refernces #14

--- albumRank_test.rb
+++ albumRank_test.rb
@@ -19,8 +19,25 @@ class AlbumsAppTest < Test::Unit::TestCase
     end
      
     @rankedAlbums = AlbumRank.build_array(albums)
-
     assert_equal 10, @rankedAlbums.length
-    # test some mroe stuff
+    
+    #Test format is correct for all
+    @rankedAlbums.each do |item|
+      assert(item.name.is_a?(String))
+      assert(item.year.to_i.is_a?(Integer))
+      assert(item.rank.to_i.is_a?(Integer))
+    end
+
+    #Test first element is like it should be
+    assert_equal("Sgt. Pepper's Lonely Hearts Club Band", @rankedAlbums.at(0).name)
+    assert_equal("1967", @rankedAlbums.at(0).year.chomp.strip)
+    assert_equal(1, @rankedAlbums.at(0).rank)
+
+    #Test last element is like it should be
+    assert_equal("The Joshua Tree", @rankedAlbums.at(9).name)
+    assert_equal("1987", @rankedAlbums.at(9).year.chomp.strip)
+    assert_equal(10, @rankedAlbums.at(9).rank)
+
+    # test some more stuff
   end 
 end
