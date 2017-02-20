d9c0ad82be7b75d040c9ff057355f823df667622
Makeila Lundy <lundym@oregonstate.edu>
2017-02-20 14:49:01 -0800

    Fixed #17 added fucntionality to make test pass for album rank should respond

--- lib/tests/album_class_test.rb
+++ lib/tests/album_class_test.rb
@@ -1,26 +1,23 @@
 ENV['RACK_ENV'] = 'test'
 
 require_relative '../main.rb'
-require 'rack/test'
 require 'test/unit'
 
 class AlbumClassTest < Test::Unit::TestCase
-  include Rack::Test::Methods
 
-  def app
-    Album.new
+  RANK = "1"
+  NAME = "Title"
+  YEAR = "2017"
+
+
+  def setup
+    @album = Album.new(RANK, NAME, YEAR)
   end
 
   def test_album_class_initializer
-  	rank = "1"
-  	name = "Title"
-  	year = "2017"
-
-  	test = Album.new(rank, name, year)
-
-  	assert_equal("Title", test.name)
-  	assert_equal("1", test.rank)
-  	assert_equal("2017", test.year)
+  	assert_equal(TITLE, @album.name)
+  	assert_equal(RANK, @album.rank)
+  	assert_equal(YEAR, @album.year)
   end
 
   def test_album_responds_to_title
